8367bcb3e33b760c03dd3080e9f6b560b5355c97
bogdan92007 <bogdan92007@gmail.com>
2021-11-24 21:43:45 +0200

    made updates to CDN and jQuery scripts for all files, added log-in and registry page

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,21 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Alibazon' });
 });
-/* GET products page */
+/* GET catalog page */
 router.get('/products-page', function(req, res, next) {
   res.render('products-page', { title: 'Alibazon' });
 });
 
 router.get('/product-page', function(req, res, next) {
   res.render('product-page', { title: 'Alibazon' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Alibazon' });
+});
+
+router.get('/log-in', function(req, res, next) {
+  res.render('log-in', { title: 'Alibazon' });
 });
 
 module.exports = router;
